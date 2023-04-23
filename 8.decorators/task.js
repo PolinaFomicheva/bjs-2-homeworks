66ea425a39255f620d8c33760ef8e517609ebbf6
Polina Fomicheva <puffyoluv@yandex.ru>
2023-04-24 00:20:58 +0300

    Восьмое задание готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
+
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(','); 
+    let objectInCache = cache.find((item) => item.hash === hash); 
+    if (objectInCache) { 
+        console.log("Из кэша: " + objectInCache.result); 
+        return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args); 
+    cache.push({
+        hash, result
+    }) ; 
+    if (cache.length > 5) { 
+      cache.shift() 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
 }
+return wrapper;
+}
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+    let timeoutId = null;
+    function wrapper(...args) {
+      wrapper.allCount += 1;
+      if(timeoutId){
+        clearInterval(timeoutId);
+      }
+      if (timeoutId === null) {
+         wrapper.count += 1;
+         func(...args);
+      }
+      timeoutId = setTimeout(() => {
+         wrapper.count += 1;
+         func(...args);
+      }, delay);
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
